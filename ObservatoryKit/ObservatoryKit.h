8bf2529fecc80b54b74a240b904592d1a48bb39d
Don <don.willems@kolonial.no>
2020-10-22 22:06:12 +0200

    Initial Commit

---
+++ ObservatoryKit/ObservatoryKit.h
@@ -0,0 +1,18 @@
+//
+//  ObservatoryKit.h
+//  ObservatoryKit
+//
+//  Created by Don Willems on 22/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObservatoryKit.
+FOUNDATION_EXPORT double ObservatoryKitVersionNumber;
+
+//! Project version string for ObservatoryKit.
+FOUNDATION_EXPORT const unsigned char ObservatoryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObservatoryKit/PublicHeader.h>
+
+
